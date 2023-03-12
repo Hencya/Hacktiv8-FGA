91ebe58c863f2cb79ec1b47c885357e0176aa0c8
Hencya <faizrofihencya@gmail.com>
2023-03-13 02:38:30 +0700

    add answer challenge-3

--- main.go
+++ main.go
@@ -6,4 +6,12 @@ import (
 
 func main() {
 	fmt.Println("Hello World")
+
+	inputText := "selamat malam"
+	mapText := make(map[string]int)
+	for _, c := range inputText {
+		fmt.Printf("%v\n", string(c))
+		mapText[string(c)]++
+	}
+	fmt.Println(mapText)
 }
